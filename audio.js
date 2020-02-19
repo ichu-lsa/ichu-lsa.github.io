823d9866d42b5b69daeb3478dc014413636167be
ichu-lsa <60521244+ichu-lsa@users.noreply.github.com>
2020-02-19 13:26:53 -0500

    adding sound

---
+++ audio.js
@@ -0,0 +1,20 @@
+// audio context vars are declared in references
+
+// start sound
+function toggle_sound() {
+	// switch between modes
+	if (sound_on) {
+		sound_on = false;
+		sound1.disconnect(volume);
+	}
+	else {
+		sound_on = true;
+		sound1.connect(volume);
+	}
+}
+
+// 1 second beep
+function beep() {
+	toggle_sound();
+	window.setTimeout(toggle_sound, 1000);
+}
