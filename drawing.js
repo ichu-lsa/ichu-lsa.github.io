0d310e20541cbc00dc0cbfccf998a46746ff84a5
Ian Chu <ichu@lsa2.com>
2020-02-20 14:16:48 -0500

    moving the button

--- drawing.js
+++ drawing.js
@@ -1,6 +1,6 @@
 function init() {
 	// attach listener to the canvas
-	console.log("Starting Up Version 1.111");
+	console.log("Starting Up Version 1.112");
 	canvas.addEventListener('pointermove', rectDrag, false);
 	canvas.addEventListener('pointerdown', rectDrag, false);
 	canvas.addEventListener('pointerup', rectDrag, false);
