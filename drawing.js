320a9d1590c2e2756e0c4b8cfef201f6dae3d4cc
Ian Chu <ichu@lsa2.com>
2020-02-20 14:21:21 -0500

    move the scaling to the container

--- drawing.js
+++ drawing.js
@@ -1,6 +1,6 @@
 function init() {
 	// attach listener to the canvas
-	console.log("Starting Up Version 1.112");
+	console.log("Starting Up Version 1.114");
 	canvas.addEventListener('pointermove', rectDrag, false);
 	canvas.addEventListener('pointerdown', rectDrag, false);
 	canvas.addEventListener('pointerup', rectDrag, false);
