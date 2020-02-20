f4360b72a6a5e561a94cb68f00b76c273fa09a99
Ian Chu <ichu@lsa2.com>
2020-02-20 14:24:40 -0500

    moving button to top-left

--- drawing.js
+++ drawing.js
@@ -1,6 +1,6 @@
 function init() {
 	// attach listener to the canvas
-	console.log("Starting Up Version 1.114");
+	console.log("Starting Up Version 1.115");
 	canvas.addEventListener('pointermove', rectDrag, false);
 	canvas.addEventListener('pointerdown', rectDrag, false);
 	canvas.addEventListener('pointerup', rectDrag, false);
