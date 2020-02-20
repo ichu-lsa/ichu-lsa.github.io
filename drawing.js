3f939dfa4725f48515ac12928d13a82be0dc6851
ichu-lsa <60521244+ichu-lsa@users.noreply.github.com>
2020-02-20 10:05:59 -0500

    1.77

--- drawing.js
+++ drawing.js
@@ -1,6 +1,6 @@
 function init() {
 	// attach listener to the canvas
-	console.log("Starting Up Version 1.75");
+	console.log("Starting Up Version 1.77");
 	canvas.addEventListener('pointermove', rectDrag, false);
 	canvas.addEventListener('pointerdown', rectDrag, false);
 	canvas.addEventListener('pointerup', rectDrag, false);
