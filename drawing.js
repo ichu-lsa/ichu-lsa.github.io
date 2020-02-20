7fbdb93114a3e0341e10b19f968de57788dc90cc
Ian Chu <ichu@lsa2.com>
2020-02-20 16:32:36 -0500

    github is still down

--- drawing.js
+++ drawing.js
@@ -1,6 +1,6 @@
 function init() {
 	// attach listener to the canvas
-	console.log("Starting Up Version 1.135");
+	console.log("Starting Up Version 1.136");
 	canvas.addEventListener('pointermove', rectDrag, false);
 	canvas.addEventListener('pointerdown', rectDrag, false);
 	canvas.addEventListener('pointerup', rectDrag, false);
