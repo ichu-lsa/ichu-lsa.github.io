f154ec5bd419b2adb0196600805dda7804162698
Ian Chu <ichu@lsa2.com>
2020-02-20 14:14:26 -0500

    trying to move button onto canvas

--- drawing.js
+++ drawing.js
@@ -1,6 +1,6 @@
 function init() {
 	// attach listener to the canvas
-	console.log("Starting Up Version 1.110");
+	console.log("Starting Up Version 1.111");
 	canvas.addEventListener('pointermove', rectDrag, false);
 	canvas.addEventListener('pointerdown', rectDrag, false);
 	canvas.addEventListener('pointerup', rectDrag, false);
