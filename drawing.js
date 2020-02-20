5d9b7b6d1562fe2256d310f423957808166ac225
Ian Chu <ichu@lsa2.com>
2020-02-20 16:56:08 -0500

    reset light in request

--- drawing.js
+++ drawing.js
@@ -1,6 +1,6 @@
 function init() {
 	// attach listener to the canvas
-	console.log("Starting Up Version 1.141");
+	console.log("Starting Up Version 1.142");
 	canvas.addEventListener('pointermove', rectDrag, false);
 	canvas.addEventListener('pointerdown', rectDrag, false);
 	canvas.addEventListener('pointerup', rectDrag, false);
