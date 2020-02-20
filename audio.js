e53faf91d1d01122dd096fae2e03bfebf091f4e7
ichu-lsa <60521244+ichu-lsa@users.noreply.github.com>
2020-02-20 10:05:29 -0500

    removing some debug

--- audio.js
+++ audio.js
@@ -15,7 +15,6 @@ function toggle_sound() {
 
 // 1 second beep
 function beep() {
-	console.log("Beep Audio: " + audio_player.state);
 	console.log("Beep Volume: " + volume.gain.value);
 	toggle_sound();
 	window.setTimeout(toggle_sound, 1000);
