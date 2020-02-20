250b89c1eb129b895dcd083e87d6ecf9a1ad62e8
Ian Chu <ichu@lsa2.com>
2020-02-20 14:31:17 -0500

    setting inner text color on button

--- drawing.js
+++ drawing.js
@@ -1,6 +1,6 @@
 function init() {
 	// attach listener to the canvas
-	console.log("Starting Up Version 1.117");
+	console.log("Starting Up Version 1.118");
 	canvas.addEventListener('pointermove', rectDrag, false);
 	canvas.addEventListener('pointerdown', rectDrag, false);
 	canvas.addEventListener('pointerup', rectDrag, false);
