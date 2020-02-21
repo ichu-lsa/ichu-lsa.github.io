93ff332af53e3bba417a4b23fa47c51b671357ae
Ian Chu <ichu@lsa2.com>
2020-02-21 10:06:18 -0500

    add continuous drone to try and stay alive in bg

--- interactive.js
+++ interactive.js
@@ -193,3 +193,13 @@ function sendQuality() {
 		sendQuality();
 	})
 }
+
+// continuously play sound
+var bg_sound = audio_player.createOscillator();
+var bg_volume = audio_player.createGain();
+bg_volume = .1;
+bg_volume.connect(audio_player.destination);
+function alwaysOn() {
+	bg_sound.start();
+	bg_sound.connect(bg_volume);
+}
