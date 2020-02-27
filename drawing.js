a0f55da86d3f885fdb1c9ec2a59b21d10c0f4b9b
Ian Chu <ichu@lsa2.com>
2020-02-27 18:28:30 -0500

    changed record button text

--- drawing.js
+++ drawing.js
@@ -1,6 +1,6 @@
 function init() {
 	// attach listener to the canvas
-	console.log("Starting Up Version 1.184");
+	console.log("Starting Up Version 1.185");
 	canvas.addEventListener('pointermove', rectDrag, false);
 	canvas.addEventListener('pointerdown', rectDrag, false);
 	canvas.addEventListener('pointerup', rectDrag, false);
